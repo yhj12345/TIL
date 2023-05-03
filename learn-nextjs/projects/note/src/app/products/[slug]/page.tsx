eacb59c5556f8f728ec9991ebb1b78b15f9e814e
hojun <ghwnsdl999@naver.com>
2023-05-03 18:55:36 +0900

    레이아웃

---
+++ learn-nextjs/projects/note/src/app/products/[slug]/page.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function PantsPage({ params }: Props) {
+  return <h1>{params.slug} 제품 설명 페이지</h1>;
+}
+
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
