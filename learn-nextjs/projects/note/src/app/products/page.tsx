333e9c212645d11cad96f379f6b01e87f7a471e9
hojun <ghwnsdl999@naver.com>
2023-05-03 19:17:17 +0900

    Link

--- learn-nextjs/projects/note/src/app/products/page.tsx
+++ learn-nextjs/projects/note/src/app/products/page.tsx
@@ -1,3 +1,31 @@
+import Link from "next/link";
+const products = ["shirt", "pants", "skirt", "shoes"];
+
 export default function ProductsPage() {
-  return <h1>제품 소개 페이지!</h1>;
+  return (
+    <div>
+      <h1>제품 소개 페이지!</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>
+            <Link href={`/products/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
+      {/* <ul>
+        <li>
+          <Link href="/products/shirt">shirt</Link>
+        </li>
+        <li>
+          <Link href="/products/pants">pants</Link>
+        </li>
+        <li>
+          <Link href="/products/skirt">skirt</Link>
+        </li>
+        <li>
+          <Link href="/products/shoes">shoes</Link>
+        </li>
+      </ul> */}
+    </div>
+  );
 }
