02d9a7b0112c0719ce58b91587ba30bc2520cd45
hojun <ghwnsdl999@naver.com>
2023-05-06 22:43:09 +0900

    서버 & 클라이언트 컴포넌트

--- learn-nextjs/projects/note/src/app/page.tsx
+++ learn-nextjs/projects/note/src/app/page.tsx
@@ -1,3 +1,13 @@
+import Counter from "@/components/Counter";
+import os from "os"; // 노드 APIS
+
 export default function Home() {
-  return <h1>홈페이지다!</h1>;
+  console.log(os.hostname());
+
+  return (
+    <>
+      <Counter />
+      <h1>홈페이지다!</h1>
+    </>
+  );
 }
