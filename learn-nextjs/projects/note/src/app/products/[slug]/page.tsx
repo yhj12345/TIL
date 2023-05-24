657009d5c66ac334f714b1e8169d5e518c8d483d
hojun <ghwnsdl999@naver.com>
2023-05-24 20:58:12 +0900

    동적으로 routing

--- learn-nextjs/projects/note/src/app/products/[slug]/page.tsx
+++ learn-nextjs/projects/note/src/app/products/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import { getProduct, getProducts } from "@/app/api/products";
+import { redirect } from "next/navigation";
 import Image from "next/image";
+import GoProductsButton from "@/components/GoProductsButton";
 
 export const revalidate = 3;
 
@@ -12,6 +14,10 @@ type Props = {
 export default async function ProductPage({ params: { slug } }: Props) {
   // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여줌
   const product = await getProduct(slug);
+
+  if (!product) {
+    redirect("/products");
+  }
   return (
     <>
       <h1>{product?.name} 제품 설명 페이지</h1>
@@ -21,6 +27,7 @@ export default async function ProductPage({ params: { slug } }: Props) {
         height={300}
         width={300}
       />
+      <GoProductsButton />
     </>
   );
 }
