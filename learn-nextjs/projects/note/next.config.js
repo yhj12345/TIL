4441a7af036f2a2e1ff82b8100f224099e3a4ad4
hojun <ghwnsdl999@naver.com>
2023-05-24 20:37:38 +0900

    redirect & rewrite

--- learn-nextjs/projects/note/next.config.js
+++ learn-nextjs/projects/note/next.config.js
@@ -11,6 +11,32 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/products/deleted_forever",
+        destination: "/products",
+        permanent: true,
+      },
+      {
+        source: "/products/deleted_temp",
+        destination: "/products",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      // {
+      //   source: "/ellie",
+      //   destination: "about/me/ellie",
+      // },
+      {
+        source: "/items/:slug",
+        destination: "/products/:slug",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
