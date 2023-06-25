84ddca9da6ee4173aa9f2326657d822c5487f417
hojun <ghwnsdl999@naver.com>
2023-06-26 00:51:47 +0900

    [FE] add : 메인 화면 프로필 컴포넌트 제작

--- learn-nextjs/projects/my-blog/src/app/page.tsx
+++ learn-nextjs/projects/my-blog/src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <section className="bg-gray-500">Main</section>;
+import Hero from "@/components/Hero";
+
+export default function HomePage() {
+  return (
+    <section>
+      <Hero />
+    </section>
+  );
 }
